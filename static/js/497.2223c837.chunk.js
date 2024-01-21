404295fa28d33cea4242f6b6241484f454b81145
NataDent <125708241+NataDent@users.noreply.github.com>
2024-01-21 01:42:03 +0000

    Deploying to gh-pages from @ NataDent/car-rental@3cbc27311f9c4e9d4ac154d98352b2e912652a25 🚀

---
+++ static/js/497.2223c837.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[497],{2497:(e,t,a)=>{a.r(t),a.d(t,{default:()=>s});var r=a(184);const s=()=>(0,r.jsx)("div",{children:"Favorites"})}}]);
+//# sourceMappingURL=497.2223c837.chunk.js.map
--- static/js/497.515b59e3.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[497],{497:(e,t,a)=>{a.r(t),a.d(t,{default:()=>s});var r=a(184);const s=()=>(0,r.jsx)("div",{children:"Favorites"})}}]);
-//# sourceMappingURL=497.515b59e3.chunk.js.map
